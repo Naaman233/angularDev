56037c21d40b762bb501acaeab3f42b83f308ed7
Makafui <makafui17@gmail.com>
2020-03-19 18:58:50 -0400

    working on recipes list display

--- app-project/src/app/recipes/recipe.model.ts
+++ app-project/src/app/recipes/recipe.model.ts
@@ -2,7 +2,8 @@ export class Recipe{
    public name: String 
    public description : String
    public imagePath: String
-
+   
+   
     constructor(name:String, desc:String,imagePath:String){
         this.name = name
         this.description = desc
